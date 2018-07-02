53444c50662f565f75a7a799141d1f9c73efa356
neeraj sah <neerajsah@qainfotech.com>
2018-07-02 16:56:48 +0530

    TatocTestNGTest
    TatocTestNGTest

---
+++ src/test/java/testing/PageObjectTatocTestNGTest.java
@@ -0,0 +1,94 @@
+package testing;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.mycompany.pageobjecttatoc.action.Pop;
+import com.mycompany.pageobjecttatoc.action.Drag;
+import com.mycompany.pageobjecttatoc.action.Frame;
+import com.mycompany.pageobjecttatoc.action.Grid;
+import com.mycompany.pageobjecttatoc.action.BasicCourse;
+import com.mycompany.pageobjecttatoc.action.CookieHandle;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+/**
+ *
+ * @author neerajsah
+ */
+public class PageObjectTatocTestNGTest 
+{
+     WebDriver driver;
+   
+    @BeforeClass
+    public void launchBrowser() throws InterruptedException
+    { 
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\neerajsah\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://10.0.1.86/tatoc");
+        
+        System.out.print(driver.getCurrentUrl());
+        Thread.sleep(0500);
+    }
+    
+     @Test
+    public void callBasic() throws InterruptedException
+    {
+        BasicCourse basic=new BasicCourse(driver);
+        basic.basicCourseSelection();
+        System.out.print(driver.getCurrentUrl());
+    }   
+        
+    @Test(dependsOnMethods={"callBasic"})
+    public void callGrid()
+    {
+        
+        Grid grid=new Grid(driver);
+        grid.gridGate();
+        System.out.print(driver.getCurrentUrl());
+    }
+    
+    @Test(dependsOnMethods={"callGrid"})
+    public void callFrame()    
+    {        
+        Frame frame=new Frame(driver);
+        frame.frameDungeon();
+        System.out.print(driver.getCurrentUrl());
+    }
+
+    @Test(dependsOnMethods={"callFrame"})
+    public void callDrag()
+    {        
+        Drag drag=new Drag(driver);
+        drag.dragAround();
+        System.out.print(driver.getCurrentUrl());
+    }  
+    
+    @Test(dependsOnMethods={"callDrag"})
+    public void callPop()
+    {        
+        Pop pop=new Pop(driver);
+        pop.popWindow();
+        System.out.print(driver.getCurrentUrl());
+    }
+
+    @Test(dependsOnMethods={"callPop"})
+    public void callCookie()
+    {
+        CookieHandle cookie=new CookieHandle(driver);
+        cookie.cookieHandling();
+        System.out.print(driver.getCurrentUrl());
+    }
+    
+   
+    
+    @AfterClass
+    public void closeBrowser()
+    { driver.quit();
+    }
+    
+}
